f6ccd6487cce7fd1cf9f96797156715850ea9aa1
MauroAntonioBogado1990 <mauroantoniobogado@gmail.com>
2025-11-13 14:01:09 -0300

    add new values in the report

--- models/res_partner.py
+++ models/res_partner.py
@@ -15,22 +15,30 @@ class ResPartner(models.Model):
 
     how_met_us_other = fields.Char("Especificar otro")
 
-    product_interests = fields.Many2many(
-    'product.category',
-    'res_partner_product_interest_rel',  # nombre único de la tabla relacional
-    'partner_id',
-    'category_id',
-    string="Productos que te interesan"
-    )
+    interest_products = fields.Selection([
+        ('accessories', 'Accesorios'), 
+        ('spare-parts', 'Repuestos'), 
+        ('other_products', 'Otro')
+    ], string="Productos que te interesan")
+    
+    how_met_us_other_products = fields.Char("Especificar otro") 
 
-    product_categories = fields.Many2many(
-        'product.category',
-        'res_partner_product_category_rel',  # nombre distinto de la tabla relacional
-        'partner_id',
-        'category_id',
-        string="Categorías de interés"
-    )
-    product_interest_other = fields.Char("Otros productos")
+    # product_interests = fields.Many2many(
+    # 'product.category',
+    # 'res_partner_product_interest_rel',  # nombre único de la tabla relacional
+    # 'partner_id',
+    # 'category_id',
+    # string="Productos que te interesan"
+    # )
+
+    # product_categories = fields.Many2many(
+    #     'product.category',
+    #     'res_partner_product_category_rel',  # nombre distinto de la tabla relacional
+    #     'partner_id',
+    #     'category_id',
+    #     string="Categorías de interés"
+    # )
+    # product_interest_other = fields.Char("Otros productos")
 
     client_type = fields.Selection([
         ('entrepreneur', 'Quiero emprender'),
@@ -54,5 +62,23 @@ class ResPartner(models.Model):
     ], string="Cantidad de locales")
 
     
-    product_categories_other = fields.Text("Otros productos")
+    #product_categories_other = fields.Text("Otros productos")
+    product_categories_other = fields.Selection(
+    [
+        ('chargers_and_cables', 'Cargadores y cables'),
+        ('cases_and_tempered_glass', 'Fundas y vidrios templados'),
+        ('headphones_and_speakers', 'Auriculares y parlantes'),
+        ('cellphone_parts', 'Repuestos de celular'),
+        ('repair_tools', 'Herramientas de reparación de celulares'),
+        ('bazaar', 'Bazar'),
+        ('hardware_store', 'Ferretería'),
+        ('solar_energy', 'Energía Solar'),
+        ('cameras_and_alarms', 'Cámaras y alarmas'),
+        ('others_products_categories', 'Otros'),
+    ],
+    string='Categoría de Producto (Otros)',
+    default='others_products_categories',
+    required=True,
+    )
+    text_others_products_categories = fields.Char("Especificar otros productos")
     additional_comments = fields.Text("Comentarios adicionales")
